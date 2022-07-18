c80eefe6b34714953c9e066a8f9934d871ba9c6f
satendrakushwaha8092 <satykush8092@gmail.com>
2022-07-18 17:38:31 +0530

    completed

--- src/route.js
+++
@@ -1,9 +0,0 @@
-const express=require('express')
-const router=express.Router()  //calling router method
-const controller=require('./controller')  //importing  controller
-
-router.post('/business-card',controller.createBusinessCard)  //creating apis and calling related method
-
-router.get('/business-card/:id',controller.getbusinesscard)
-
-module.exports=router  //exporting router
